351173af71961e9a974bf308299c9ab1c937dad8
vanellope <phamhung20022015@gmail.com>
2024-11-07 22:04:58 +0700

    update

--- agent.py
+++ agent.py
@@ -11,15 +11,22 @@ BATCH_SIZE = 1000
 LR = 0.001
 
 class Agent:
-
-    def __init__(self):
+    def __init__(self, epsilon_start=1.0, epsilon_end=0.01, epsilon_decay=0.995):
         self.n_games = 0
-        self.epsilon = 0 # randomness
-        self.gamma = 0.9 # discount rate
-        self.memory = deque(maxlen=MAX_MEMORY) # popleft()
+        self.gamma = 0.9  # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY)
         self.model = Linear_QNet(11, 256, 3)
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
-
+        
+        # Set the device for tensor computation
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
+        
+        # Epsilon parameters for exploration
+        self.epsilon = epsilon_start
+        self.epsilon_min = epsilon_end
+        self.epsilon_decay = epsilon_decay
+        self.n_games = 0
 
     def get_state(self, game):
         head = game.snake[0]
@@ -27,7 +34,7 @@ class Agent:
         point_r = Point(head.x + 20, head.y)
         point_u = Point(head.x, head.y - 20)
         point_d = Point(head.x, head.y + 20)
-        
+
         dir_l = game.direction == Direction.LEFT
         dir_r = game.direction == Direction.RIGHT
         dir_u = game.direction == Direction.UP
@@ -62,43 +69,56 @@ class Agent:
             game.food.x < game.head.x,  # food left
             game.food.x > game.head.x,  # food right
             game.food.y < game.head.y,  # food up
-            game.food.y > game.head.y  # food down
-            ]
+            game.food.y > game.head.y   # food down
+        ]
 
         return np.array(state, dtype=int)
 
     def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached
+        self.memory.append((state, action, reward, next_state, done))
 
     def train_long_memory(self):
         if len(self.memory) > BATCH_SIZE:
-            mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples
+            mini_sample = random.sample(self.memory, BATCH_SIZE)
         else:
             mini_sample = self.memory
 
         states, actions, rewards, next_states, dones = zip(*mini_sample)
+        
+        # Properly convert numpy arrays to tensors
+        states = torch.FloatTensor(np.array(states)).to(self.device)
+        next_states = torch.FloatTensor(np.array(next_states)).to(self.device)
+        actions = torch.LongTensor(np.array(actions)).to(self.device)
+        rewards = torch.FloatTensor(np.array(rewards)).to(self.device)
+        dones = torch.FloatTensor(np.array(dones)).to(self.device)
+
         self.trainer.train_step(states, actions, rewards, next_states, dones)
-        #for state, action, reward, nexrt_state, done in mini_sample:
-        #    self.trainer.train_step(state, action, reward, next_state, done)
 
     def train_short_memory(self, state, action, reward, next_state, done):
+        # Convert inputs to tensors properly
+        state = torch.FloatTensor(np.array([state])).to(self.device)
+        next_state = torch.FloatTensor(np.array([next_state])).to(self.device)
+        action = torch.LongTensor(np.array([action])).to(self.device)
+        reward = torch.FloatTensor(np.array([reward])).to(self.device)
+        done = torch.FloatTensor(np.array([done])).to(self.device)
+        
         self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
-        # random moves: tradeoff exploration / exploitation
-        self.epsilon = 80 - self.n_games
-        final_move = [0,0,0]
-        if random.randint(0, 200) < self.epsilon:
+        # Update epsilon with decay
+        self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
+
+        if random.random() < self.epsilon:
             move = random.randint(0, 2)
-            final_move[move] = 1
         else:
-            state0 = torch.tensor(state, dtype=torch.float)
+            # Convert state to tensor properly
+            state0 = torch.FloatTensor(state).unsqueeze(0).to(self.device)
             prediction = self.model(state0)
             move = torch.argmax(prediction).item()
-            final_move[move] = 1
 
+        final_move = [0, 0, 0]
+        final_move[move] = 1
         return final_move
-
 
 def train():
     plot_scores = []
@@ -107,25 +127,17 @@ def train():
     record = 0
     agent = Agent()
     game = SnakeGameAI()
+    
     while True:
-        # get old state
         state_old = agent.get_state(game)
-
-        # get move
         final_move = agent.get_action(state_old)
-
-        # perform move and get new state
         reward, done, score = game.play_step(final_move)
         state_new = agent.get_state(game)
 
-        # train short memory
         agent.train_short_memory(state_old, final_move, reward, state_new, done)
-
-        # remember
         agent.remember(state_old, final_move, reward, state_new, done)
 
         if done:
-            # train long memory, plot result
             game.reset()
             agent.n_games += 1
             agent.train_long_memory()
@@ -141,7 +153,6 @@ def train():
             mean_score = total_score / agent.n_games
             plot_mean_scores.append(mean_score)
             plot(plot_scores, plot_mean_scores)
-
 
 if __name__ == '__main__':
     train()
